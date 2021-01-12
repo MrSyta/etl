863793610c9e8c3648b60f7224506d11b4c5ddd7
Marcin <gamer19@poczta.fm>
2021-01-12 15:20:30 +0100

    feat(transform): implement saving transformed data to new json file

--- transform/transform.py
+++ transform/transform.py
@@ -1,10 +1,12 @@
 import json
-import datetime
 
 with open("../extract/data.json") as file:
     data = json.load(file)
-    for currency in data:
-        currency["exchange"] = float(currency["exchange"].replace(",", "."))
-        currency["transition"] = float(currency["transition"].rstrip("%"))
-        currency["transition_pln"] = float(currency["transition_pln"].rstrip("zł"))
-        currency["date"] = datetime.datetime.strptime(currency["date"], "%Y-%m-%d")
+
+for currency in data:
+    currency["exchange"] = float(currency["exchange"].replace(",", "."))
+    currency["transition"] = float(currency["transition"].rstrip("%"))
+    currency["transition_pln"] = float(currency["transition_pln"].rstrip("zł"))
+
+with open("transformed_data.json", "w") as file:
+    json.dump(data, file)

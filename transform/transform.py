b25dfb25fc97f916fa52a3207d3a0de0afac3344
Marcin <gamer19@poczta.fm>
2021-01-13 12:34:56 +0100

    feat(transform): implement currency_name striping

--- transform/transform.py
+++ transform/transform.py
@@ -1,12 +1,15 @@
 import json
 
-with open("../extract/data.json") as file:
-    data = json.load(file)
 
-for currency in data:
-    currency["exchange"] = float(currency["exchange"].replace(",", "."))
-    currency["transition"] = float(currency["transition"].rstrip("%"))
-    currency["transition_pln"] = float(currency["transition_pln"].rstrip("zł"))
+def transform():
+    with open("../extract/data.json") as file:
+        data = json.load(file)
 
-with open("transformed_data.json", "w") as file:
-    json.dump(data, file)
+    for currency in data:
+        currency["currency_name"] = currency["currency_name"].strip()
+        currency["exchange"] = float(currency["exchange"].replace(",", "."))
+        currency["transition"] = float(currency["transition"].rstrip("%"))
+        currency["transition_pln"] = float(currency["transition_pln"].rstrip("zł"))
+
+    with open("transformed_data.json", "w") as file:
+        json.dump(data, file)
